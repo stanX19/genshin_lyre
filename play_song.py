ee7b5bdfb5ec025943caae384b0a5f9c9f973d60
stan <tanshanchien@gmail.com>
2022-10-16 01:47:50 +0800

    fixed loop bug, added notif

--- play_song.py
+++ play_song.py
@@ -1,6 +1,14 @@
 from classes import *
 from utils import notify
 
+def notify_current_song(song_idx):
+    all_songs = list(Songs.songs.keys())
+    next_idx = song_idx + 1 if song_idx + 1 != len(all_songs) else 0
+    last_idx = song_idx - 1 if song_idx > 0 else len(all_songs) - 1
+    notify(f"({last_idx + 1}) {all_songs[last_idx]}\n\
+({song_idx + 1}) {all_songs[song_idx]}           (currently playing)\n\
+({next_idx + 1}) {all_songs[next_idx]}")
+
 def play_song(song_idx):
     print(f"currently playing: {list(Songs.songs.keys())[song_idx]} ({song_idx + 1})")
     if Settings.notification:
@@ -13,7 +21,8 @@ def play_song(song_idx):
             last_song = list(Songs.songs.keys())[song_idx - 1]
         except IndexError:
             last_song = list(Songs.songs.keys())[len(Songs.songs) - 1]
-        notify(f"currently playing: {current_song}\npre: {last_song}\nnext: {next_song}")
+        if Settings.notification:
+            notify_current_song(song_idx)
     # time.sleep(1)
     pyautogui.click(Settings.genshin_app_coordinate)
     last_action = time.time()
@@ -34,86 +43,43 @@ def play_song(song_idx):
         if keyboard.is_pressed("=") or time.time() >= last_action + 5:
             keyboard.wait("shift")
             last_action = time.time()
-
+        if keyboard.is_pressed("n"):
+            if Settings.notification:
+                notify_current_song(song_idx)
+            while keyboard.is_pressed("n"):
+                pass
         if UserVaria.song_loop and PlayVaria.allow_autoplay:
+            if keyboard.is_pressed("shift"):
+                PlayVaria.allow_autoplay = False
+                continue
             song_idx = (song_idx + 1) if song_idx < len(Songs.songs) - 1 else 0
             print(
                 f"currently playing: {list(Songs.songs.keys())[song_idx]} ({song_idx + 1})")
 
-            # pyautogui.press("win")
-            # time.sleep(0.1)
-            # pyautogui.press("win")
             if Settings.notification:
-                current_song = list(Songs.songs.keys())[song_idx]
-                try:
-                    next_song = list(Songs.songs.keys())[song_idx + 1]
-                except IndexError:
-                    next_song = list(Songs.songs.keys())[0]
-                try:
-                    last_song = list(Songs.songs.keys())[song_idx - 1]
-                except IndexError:
-                    last_song = list(Songs.songs.keys())[len(Songs.songs) - 1]
-                notify(f"currently playing: {current_song}\npre: {last_song}\nnext: {next_song}")
-                # time.sleep(0.3)
-            # pyautogui.click(Settings.genshin_app_coordinate)
-            # for i in range(260):
-            #     time.sleep(0.01)
-            #     if keyboard.is_pressed("shift"):
-            #         PlayVaria.allow_autoplay = False
-            #         break
+                notify_current_song(song_idx)
+
+            for i in range(260):
+                time.sleep(0.01)
+                if keyboard.is_pressed("shift"):
+                    PlayVaria.allow_autoplay = False
+                    break
             PlayVaria.song_index = 0
 
         if keyboard.is_pressed(".") or keyboard.is_pressed(","):
             if keyboard.is_pressed("."):
-                while keyboard.is_pressed("."):
-                    pass
                 song_idx = (song_idx + 1) if song_idx < len(
                     Songs.songs) - 1 else 0
 
             elif keyboard.is_pressed(","):
-                while keyboard.is_pressed(","):
-                    pass
                 song_idx = (song_idx - 1) if song_idx != 0 else len(
                     Songs.songs) - 1
 
-            print(
-                f"currently playing: {list(Songs.songs.keys())[song_idx]} ({song_idx + 1})")
-
-            # keyboard.block_key(",")  # prevent typing in search bar
-            # keyboard.block_key(".")
-            # pyautogui.press("win") #back to desktop
-            # time.sleep(0.1)
-            # pyautogui.press("win")
-            # keyboard.unhook_all()  # unlock the blocks
-
-            # escape_time = time.time() + 0  # time before confirming what songs to play
-            # while time.time() < escape_time:
-            #
-            #     if keyboard.is_pressed("."):
-            #         while keyboard.is_pressed("."):
-            #             pass
-            #         song_idx = (song_idx + 1) if song_idx < (
-            #                     len(Songs.songs) - 1) else 0
-            #         escape_time = time.time() + 1
-            #         print(
-            #             f"currently playing: {list(Songs.songs.keys())[song_idx]} ({song_idx + 1})")
-            #     elif keyboard.is_pressed(","):
-            #         while keyboard.is_pressed(","):
-            #             pass
-            #         song_idx = (song_idx - 1) if song_idx != 0 else len(
-            #             Songs.songs) - 1
-            #         escape_time = time.time() + 1
-            #         print(
-            #             f"currently playing: {list(Songs.songs.keys())[song_idx]} ({song_idx + 1})")
-
+            print(f"currently playing: {list(Songs.songs.keys())[song_idx]} ({song_idx + 1})")
             if Settings.notification:
-                all_songs = list(Songs.songs.keys())
-                next_idx = song_idx + 1 if song_idx + 1 != len(all_songs) else 0
-                last_idx = song_idx - 1 if song_idx > 0 else len(all_songs) - 1
-                notify(f"({last_idx + 1}) {all_songs[last_idx]}\n\
-({song_idx + 1}) {all_songs[song_idx]}           (currently playing)\n\
-({next_idx + 1}) {all_songs[next_idx]}")
-                # time.sleep(0.3)
-            # pyautogui.click(Settings.genshin_app_coordinate)
+                notify_current_song(song_idx)
             PlayVaria.song_index = 0
+
+            while keyboard.is_pressed(".") or keyboard.is_pressed(","):
+                pass
             last_action = time.time()
