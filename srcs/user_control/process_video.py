607186ff678e22b13614e142a2010a02afaf7ccc
StanX19 <tanshanchien@gmail.com>
2024-11-20 16:06:03 +0800

    Bug fix
    prevented error caused by mp4_to_lyre_score being uninitialized

--- srcs/user_control/process_video.py
+++ srcs/user_control/process_video.py
@@ -8,6 +8,16 @@ try:
 except ImportError as exc:
     def decorator(exc):
         def func(save_dir: str, vid_path: list[str]):
-            utils.print_cmd_color("DarkYellow", f"Failed to import 'lyre_mp4_to_score' module\nError: {exc}")
+            utils.print_cmd_color("DarkYellow",
+                                  f"Failed to import 'lyre_mp4_to_score' module\n    Error: {exc}")
+            print()
+        return func
+    save_video_as_nightly = decorator(exc)
+except AttributeError as exc:
+    def decorator(exc):
+        def func(save_dir: str, vid_path: list[str]):
+            utils.print_cmd_color("DarkYellow",
+                                  f"Error: 'lyre_mp4_to_score' module is not available")
+            print()
         return func
     save_video_as_nightly = decorator(exc)
